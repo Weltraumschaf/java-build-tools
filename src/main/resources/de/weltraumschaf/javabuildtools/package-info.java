fed406a9bb4f178724fe6c5c24acbb6b73bfa851
Sven Strittmatter <ich@weltraumschaf.de>
2014-04-19 13:45:36 +0200

    Initial commit.

---
+++ src/main/resources/de/weltraumschaf/javabuildtools/package-info.java
@@ -0,0 +1,22 @@
+/*
+ *  LICENSE
+ *
+ * "THE BEER-WARE LICENSE" (Revision 43):
+ * "Sven Strittmatter" <weltraumschaf@googlemail.com> wrote this file.
+ * As long as you retain this notice you can do whatever you want with
+ * this stuff. If we meet some day, and you think this stuff is worth it,
+ * you can buy me a non alcohol-free beer in return.
+ *
+ * Copyright (C) 2012 "Sven Strittmatter" <weltraumschaf@googlemail.com>
+ */
+
+/**
+ * Everything to support build process.
+ *
+ * Contains:
+ * <ul>
+ * <li>Checkstyle configuration</li>
+ * <li>PMD rule set</li>
+ * </ul>
+ */
+package de.weltraumschaf.javabuildtools;
